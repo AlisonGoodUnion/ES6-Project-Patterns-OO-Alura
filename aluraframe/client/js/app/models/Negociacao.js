4d8512abf66b17ec76dcda052829a8cb6a340f49
Alison <alisonal1@hotmail.com>
2017-11-18 15:50:16 -0200

    02 A classe Negociação

---
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -0,0 +1,11 @@
+/**
+ * Created by alison on 18/11/2017.
+ */
+class Negociacao {
+
+    constructor(){
+        this.data = new Date();
+        this.valor = 0.0;
+        this.quantidade = 0;
+    }
+}
