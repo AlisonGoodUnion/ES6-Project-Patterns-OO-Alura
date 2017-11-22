dd28cb3ffd99206b7e5452eee1dec2401054b590
Alison Ramos da Silva <alisonal1@hotmail.com>
2017-11-21 22:05:45 -0200

    08 Programação defensiva

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -4,9 +4,10 @@
 class Negociacao {
 
     constructor(data, quantidade, valor){
-        this._data = data;
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
+        Object.freeze(this);
     }
 
     getVolume(){
