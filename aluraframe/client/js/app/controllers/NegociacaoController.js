ad8175e479bf65b70a949379c6a1647e164e4ce4
Alison Ramos da Silva <alisonal1@hotmail.com>
2017-11-29 22:05:06 -0200

    05 Criando um objeto Date

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -2,16 +2,28 @@ class NegociacaoController {
 
     constructor() {
         let $ = document.querySelector.bind(document);
-        this.inputData = $('#data');
-        this.inputQuantidade = $('#quantidade');
-        this.inputValor = $('#valor');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
     }
 
     adiciona(event){
         event.preventDefault();
 
-        console.log(this.inputData.value);
-        console.log(this.inputQuantidade.value);
-        console.log(this.inputValor.value);
+        let data = this._inputData.value;
+        let dataSplit = this._inputData.value.split('-');
+        let dataReplace = this._inputData.value.replace(/-/g, ',');
+
+        console.log(data);
+        console.log(dataSplit);
+        console.log(dataReplace);
+
+        let negociacao = new Negociacao(
+            this._inputData.value,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
     }
 }
